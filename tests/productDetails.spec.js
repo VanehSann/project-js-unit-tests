6898c2f7ba546a63273505b044af258dfe7d80d4
VanehSann <vanessacbsantos22@gmail.com>
2022-01-18 05:06:16 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(typeof productDetails().entries()).toBe('object');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails().length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('comida', 'casa').keys()).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    // If it should pass with deep equality, replace "toBe" with "toStrictEqual"
+    expect(productDetails('lado', 'lados')).toStrictEqual([{"details": {"productId": "lado123"}, "name": "lado"}, {"details": {"productId": "lados123"}, "name": "lados"}]);
+    expect(productDetails('ladorA', 'ladosA')).toStrictEqual([{"details": {"productId": "ladorA123"}, "name": "ladorA"}, {"details": {"productId": "ladosA123"}, "name": "ladosA"}]);
     // Teste se os dois productIds terminam com 123.
+    // esse codigo também valida se está igual
+    expect(productDetails('euAmo', 'comer')).toStrictEqual([{"details": {"productId": "euAmo123"}, "name": "euAmo"}, {"details": {"productId": "comer123"}, "name": "comer"}]);
   });
 });
